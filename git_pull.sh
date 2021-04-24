38ace2a605218cd746433e0c69bf55e81b86b886
zhangjt <zhangjinteng163@gmail.com>
2021-04-24 23:28:38 +0800

    fix: 修改script地址

--- git_pull.sh
+++ git_pull.sh
@@ -19,7 +19,7 @@ ContentVersion=${ShellDir}/version
 ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
-ScriptsURL=https://github.com/RikudouPatrickstar/jd_scripts/tree/master
+ScriptsURL=https://github.com/l499477004/JD-scripts.git
 ShellURL=https://github.com/zhangkuang/jd-base
 
 
